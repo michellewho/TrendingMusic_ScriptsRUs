cc794814a79456e4802cfcc714f62e1c70371dcc
MacKenzieO <mackenzieolson15@gmail.com>
2017-11-02 19:20:14 -0700

    Add files via upload

---
+++ Spotify_Api_scraping.py
@@ -0,0 +1,66 @@
+import spotipy
+from pandas.io.json import json_normalize
+import requests
+import json
+import pandas as pd
+# import top_100 from Jons_Script
+
+api_token = "BQBcGIyFE-zGL7H3-KlhRTrFX83dYbIk3Pc_zeoraZmqs_MgXNmttl8c2DxXAa9E0QsQnl2GyJFCjaKdkYMiScjj9ZPfNb7RumC-ufNFS59ur6nyG3QqLlM_CZ5YJw0Z9JhkyQioUk5_SWCnKy7jrURpbPrbne3Znp4"
+
+# How to get information for a SINGLE song
+def getAudioFeatures(track_Id, api_token): 
+	spotify = spotipy.Spotify(auth=api_token)
+	uri = 'spotify:track:' + track_Id
+	results = spotify.audio_features(uri)
+	song_df = json_normalize(results)
+	return song_df
+
+# How to get trackId for a song
+def getTrackId(song_title, api_token):
+	api_url_base = "https://api.spotify.com/v1/search"
+	query_params = "?q=" + song_title + "&type=track&market=US&limit=1"
+	headers = {'Content-type': 'application/json',
+				'Authorization': 'Bearer {0}'.format(api_token)}
+	response = requests.get(api_url_base + query_params, headers = headers)
+
+	if response.status_code == 200:
+		json_info = json.loads(response.content.decode('utf-8'))
+		temp = json_info['tracks']['items']
+		if len(temp) != 0: 
+			dictionary = temp[0]
+			return dictionary['id']
+		else:
+			return None
+	else:
+		return None 
+
+# How to get the DF of Spotify info 
+def getDataframeForSingleSong(df, index, api_token):
+	name = df.get_value(index, "SongTitle")
+	name_altered = name.replace(" ", "+")
+	artist = df.get_value(index, "Artist")
+	artist_altered = artist.replace(" ", "+")
+	trackId = getTrackId(name_altered, api_token)
+	if trackId != None: 
+		df_individual = getAudioFeatures(trackId, api_token)
+		df_individual['song_title'] = name
+		df_individual['artist_name'] = artist
+		return df_individual
+	else: 
+		return None
+
+# Main
+unique_songs = pd.read_csv('Unique_Song_list_1year.csv')
+size = len(unique_songs.index)
+
+
+df_final = getDataframeForSingleSong(unique_songs, 0, api_token)
+
+for x in range(2, size):
+	df_individual = getDataframeForSingleSong(unique_songs, x, api_token)
+	if df_individual is None: 
+		pass
+	else: 
+		df_final = df_final.append(df_individual)
+
+df_final.to_csv("spotify_data.csv")
