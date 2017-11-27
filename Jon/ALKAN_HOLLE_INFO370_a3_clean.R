592a144cf43ffc02e08790c7da2dca3ea6cda321
Jonathan Alkan <jonalkan@gmail.com>
2017-11-27 10:56:02 -0800

    newest clean

--- Jon/ALKAN_HOLLE_INFO370_a3_clean.R
+++ Jon/ALKAN_HOLLE_INFO370_a3_clean.R
@@ -6,22 +6,34 @@ setwd("~/Desktop/INFO370")
 stravaData <- read.csv("strava_activity.csv")
 
 
-# Noticed the only workouts with significant numbers were Rides and Runs
+# A look into the most popular workouts
 typeCount <- group_by(cleanStravaData, type) %>%
-  summarise(averageSpeed = mean(average_speed), averageDistance = mean(distance), score = (averageSpeed * averageDistance), n = n())
-#View(typeCount)
+  summarise(n = n())
+View(typeCount)
+
+# A look into the countries with the most workouts recorded 
+countryCount <- group_by(cleanStravaData, athlete.country) %>%
+  summarise(n = n())
+View(countryCount)
 
 # General filters that apply to both data sets 
-stravaData <- filter(stravaData, total_elevation_gain < 8000, distance < 600000, resource_state == 2, moving_time > 0, distance > 0, max_speed > 0, average_speed > 0, max_speed < 33, average_speed < max_speed, 
-                     type == "Ride" && max_speed < 33 && distance < 600000 || 
-                     type == "Run" && max_speed < 9.8 && distance < 300000)
+stravaData <- filter(stravaData, resource_state == 2, moving_time > 0, distance > 0, max_speed > 0, average_speed > 0, average_speed < max_speed, 
+                     type == "Ride" && max_speed < 33 && distance < 600000 | 
+                     type == "Run" && max_speed < 9.8 && distance < 300000,
+                     type == "Ride" | type == "Run")
 
 # Filter and select data that is relevant to questions 1
 stravaData1 <- filter(stravaData, is.null(athlete.sex) == FALSE, athlete.sex != "")
 stravaData1 <- subset(stravaData1, select = c(athlete.sex, type, average_speed, distance, total_elevation_gain))
 
 # Filter and select data that is relevant to questions 2
-stravaData2 <- filter(stravaData, is.null(athlete.country) == FALSE, athlete.country != "")
+stravaData2 <- filter(stravaData,  
+                      athlete.country == 'Australia' |
+                      athlete.country == 'Brazil' |
+                      athlete.country == 'Canada' |
+                      athlete.country == 'France' |
+                      athlete.country == 'United Kingdom' |
+                      athlete.country == 'United States')
 stravaData2 <- subset(stravaData, select = c(type, athlete.country, total_elevation_gain, distance))
 
 
